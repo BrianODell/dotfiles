54c843badeb7283d5ad8cd6733c7868c4a055796
Brian O'Dell <brian.odell@nextgearcapital.com>
2016-04-19 15:51:28 -0400

    cleanup

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 pip2 install --user powerline-status --editable ./powerline
 ln -s ~/dotfiles/powerline/scripts/powerline/powerline ~/dotfiles/bin/
-ln -s ~/dotfiles/powerline/scripts/powerline/powerline-daemon ~/dotfiles/bin/
 
 echo ". ~/dotfiles/bash_profile_continued.sh" >> ~/.bash_profile
